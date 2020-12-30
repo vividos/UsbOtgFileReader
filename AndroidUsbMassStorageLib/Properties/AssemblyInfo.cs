d5ffdcef6caef9e254ef12b6ec92fd3e1323d1f4
Michael Fink <michael.fink@asamnet.de>
2020-12-30 20:51:18 +0100

    added assembly versioning using Nerdbank.GitVersioning

--- AndroidUsbMassStorageLib/Properties/AssemblyInfo.cs
+++ AndroidUsbMassStorageLib/Properties/AssemblyInfo.cs
@@ -10,6 +10,3 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-
-[assembly: AssemblyVersion("0.7.7.0")]
-[assembly: AssemblyFileVersion("0.7.7.0")]
--- UsbOtgFileReader/Properties/AssemblyInfo.cs
+++ UsbOtgFileReader/Properties/AssemblyInfo.cs
@@ -1,9 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("USB OTG File Reader")]
 [assembly: AssemblyDescription("Reads files from USB mass storage devices using USB OTG cables")]
 [assembly: AssemblyConfiguration("")]
@@ -13,12 +10,3 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
